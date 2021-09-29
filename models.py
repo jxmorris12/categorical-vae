d803674f73565fd42b3316fe522c38d3e347c8cd
Jack Morris <jxmorris12@gmail.com>
2021-09-29 11:29:34 -0400

    encoder and decoder cnn; learning rate and temp scheduling; plot images to file; add wandb plotting

--- models.py
+++ models.py
@@ -50,17 +50,27 @@ def gumbel_softmax(logits: torch.Tensor, temperature: float, hard=False) -> torc
 
 class Encoder(torch.nn.Module):
     cnn: torch.nn.Module
+    input_shape: torch.Size
     N: int # number of categorical distributions
     K: int # number of classes
-    def __init__(self, N: int, K: int):
+    def __init__(self, N: int, K: int, input_shape: torch.Size):
         super().__init__()
         self.N = N
         self.K = K
-
-        print(f'initializing encoder with  N={N} and K={K}')
-        print(f'using MobileNetV3 encoder with {N*K} classes')
-        self.cnn = torchvision.models.mobilenet_v3_small(
-            pretrained=False, num_classes=N*K
+        self.input_shape = input_shape
+        print('N =',N,'and K =', K)
+        self.network = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 8, 3, stride=2, padding=1),
+            torch.nn.ReLU(True),
+            torch.nn.Conv2d(8, 16, 3, stride=2, padding=1),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.ReLU(True),
+            torch.nn.Conv2d(16, 32, 3, stride=2, padding=0),
+            torch.nn.ReLU(True),
+            torch.nn.Flatten(),
+            torch.nn.Linear(3 * 3 * 32, 128),
+            torch.nn.ReLU(True),
+            torch.nn.Linear(128, N*K)   
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -70,8 +80,8 @@ class Encoder(torch.nn.Module):
         """
         assert len(x.shape) == 4 # x should be of shape [B, C, Y, X]
         B = x.shape[0] # store batch dimension
-        x = x.repeat(1, 3, 1, 1) # add fake channels
-        phi = torch.reshape(self.cnn(x), (B, self.N, self.K))
+        phi = self.network(x)
+        phi = torch.reshape(phi, (B, self.N, self.K))
         return torch.sigmoid(phi) # TODO ensure sigmoid dim is correct
 
 
@@ -84,18 +94,25 @@ class Decoder(torch.nn.Module):
         self.N = N
         self.K = K
         self.output_shape = output_shape
-        # TODO make a smarter decoder with deconv?
-        self.ff = torch.nn.Linear(
-            N*K,
-            torch.prod(torch.tensor(output_shape))
+        self.network = torch.nn.Sequential(
+            torch.nn.ConvTranspose2d(1, 16, 3, stride=2, output_padding=0),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.ReLU(True),
+            torch.nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),
+            torch.nn.BatchNorm2d(8),
+            torch.nn.ReLU(True),
+            torch.nn.ConvTranspose2d(8, 1, 3, stride=2, padding=1, output_padding=1)
         )
 
     def forward(self, z: torch.Tensor) -> torch.Tensor:
         """Produces output spectrogram `x` for input `z`."""
         assert len(z.shape) == 3 # [B, N, K]
         assert z.shape[1:] == (self.N, self.K)
+        B = z.shape[0]
+        z = z.reshape((B, 1, self.N, self.K))
 
-        x_hat = self.ff(torch.reshape(z, (-1, self.N*self.K)))
+        x_hat = self.network(z)
+        breakpoint()
         x_hat = torch.reshape(x_hat, [-1] + list(self.output_shape))
         return torch.sigmoid(x_hat)
 
@@ -103,12 +120,14 @@ class Decoder(torch.nn.Module):
 class BernoulliVAE(torch.nn.Module):
     encoder: torch.nn.Module
     decoder: torch.nn.Module
+    temperature: float
     def __init__(self, encoder: torch.nn.Module, decoder: torch.nn.Module):
         super().__init__()
         self.encoder = encoder
         self.decoder = decoder
+        self.temperature = 1.0
 
-    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, x: torch.Tensor, temperature: float = 1.0) -> Tuple[torch.Tensor, torch.Tensor]:
         """VAE forward pass. Encoder produces phi, the parameters of a bernoulli distribution.
         Samples from bernoulli(phi) to produce a z. Passes z through encoder p(x|z) to get x_hat,
         a reconstruction of x.
@@ -118,13 +137,21 @@ class BernoulliVAE(torch.nn.Module):
             x_hat: auto-encoder reconstruction of x
         """
         phi = self.encoder(x)
-        print('phi.shape:', phi.shape)
-        # TODO anneal temp during training?
+        # print('phi.shape:', phi.shape)
         # z_given_x = torch.nn.functional.gumbel_softmax(phi, tau=1, hard=False)
-        #phi = torch.stack((phi, 1-phi), dim=-1) # split into 2 classes
-        z_given_x = gumbel_softmax(phi, 1.0, hard=False)
-        print('z_given_x.shape', z_given_x.shape)
+        # phi = torch.stack((phi, 1-phi), dim=-1) # split into 2 classes
+        z_given_x = gumbel_softmax(phi, temperature, hard=False)
+        # print('z_given_x.shape', z_given_x.shape)
         x_hat = self.decoder(z_given_x)
-        print('x_hat.shape:', x_hat.shape)
+        # print('x_hat.shape:', x_hat.shape)
         return phi, x_hat
-
+    
+    def generate_random_image(self, N: int, K: int, temperature: float = 1.0) -> torch.Tensor:
+        # logging: and generate random image
+        batch_size = 1
+        random_phi = torch.randn((batch_size, N, K))
+        # TODO what temperature here? hard=True or no?
+        z_given_x = gumbel_softmax(random_phi, temperature, hard=False)
+        with torch.no_grad():
+            random_image = self.decoder(z_given_x)[0] # take first of batch (one image)
+        return random_image
--- train.py
+++ train.py
@@ -1,10 +1,19 @@
 import math
+import numpy as np
+import os
 import torch
 import tqdm
+import wandb
+from PIL import Image
 
 import torch.optim as optim
 
 from models import Encoder, Decoder, BernoulliVAE
+
+wandb_run = wandb.init(
+    entity="jack-morris",
+    project="categorical-vae",
+)
 
 def load_training_data():
     import torchvision
@@ -28,8 +37,25 @@ def bernoulli_kl_divergence_canonical(phi: torch.Tensor) -> torch.Tensor:
     p = dist.Bernoulli(0.5)
     return dist.kl.kl_divergence(q, p)
 
+def create_random_image(model: BernoulliVAE, N: int, K: int, temperature: float, step: int, output_dir: str) -> Image:
+    random_image = model.generate_random_image(N, K, temperature=temperature)
+    pil_image = make_pil_image(random_image)
+    pil_image.save(os.path.join(output_dir, f"random_step_{step}.png"))
+    return pil_image
+
+def make_pil_image(img_tensor: torch.Tensor) -> Image:
+    img_tensor = img_tensor.detach().numpy().squeeze()
+    random_image = (img_tensor * 255).astype(np.uint8)
+    return Image.fromarray(random_image)
+
 def main() -> None:
-    batch_size = 32
+    batch_size = 100
+    max_steps = 50_000
+    num_epochs = 10
+    learning_rate = 0.001
+    initial_temperature = 1.0
+    minimum_temperature = 0.5
+    temperature_anneal_rate = 0.00003
     K = 10 # number of classes
     N = 30 # number of categorical distributions
 
@@ -40,25 +66,57 @@ def main() -> None:
         shuffle=True
     )
 
-    encoder = Encoder(N, K)
     image_shape = next(iter(train_dataset))[0][0].shape # [1, 28, 28]
+    encoder = Encoder(N, K, image_shape)
     decoder = Decoder(N, K, image_shape)
     model = BernoulliVAE(encoder, decoder)
 
-    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
-    for data in tqdm.tqdm(train_dataset, desc='Training'): # x should be a batch of torch.Tensor spectrograms, of shape [B, F, T]
-        x, labels = data
-        phi, x_hat = model(x)
-        # recon_loss = torch.mean(torch.sum((x - x_hat) ** 2, dim=[1,2]))
-        recon_loss = torch.mean((x - x_hat) ** 2)
-        # kl_loss = torch.mean(bernoulli_kl_divergence_canonical(phi))
-        kl_loss = torch.mean(bernoulli_kl_divergence(phi))
-        loss = kl_loss + recon_loss
-        # loss = kl_loss
-        # loss = recon_loss
-        tqdm.tqdm.write(f'Recon. loss = {recon_loss} / KL loss = {kl_loss}')
-        loss.backward()
-        optimizer.step()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+    learning_rate_scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+    step = 0
+    temperature = initial_temperature
+
+    # make folder for images
+    output_dir = os.path.join('outputs', wandb_run.id)
+    os.makedirs(output_dir)
+
+    progress_bar = tqdm.tqdm(total=max_steps, desc='Training')
+    while step < max_steps:
+        for data in train_dataset: # x should be a batch of torch.Tensor spectrograms, of shape [B, F, T]
+            x, labels = data
+            phi, x_hat = model(x, temperature)
+            # reconstruction_loss = torch.mean(torch.sum((x - x_hat) ** 2, dim=[1,2]))
+            reconstruction_loss = torch.mean((x - x_hat) ** 2)
+            # kl_loss = torch.mean(bernoulli_kl_divergence_canonical(phi))
+            kl_loss = torch.mean(bernoulli_kl_divergence(phi))
+            loss = kl_loss + reconstruction_loss
+            # loss = kl_loss
+            # loss = recon_loss
+            progress_bar.set_description(f'Training | Recon. loss = {reconstruction_loss:.7f} / KL loss = {kl_loss:.7f}')
+            loss.backward()
+            optimizer.step()
+            # Incrementally anneal temperature and learning rate.
+            if step % 1000 == 1:
+                temperature = np.maximum(initial_temperature*np.exp(-temperature_anneal_rate*step), minimum_temperature)
+                learning_rate_scheduler.step() # should multiply learning rate by 0.9
+            # phi.shape: torch.Size([256, 30, 10])
+            random_image = create_random_image(model, N, K, temperature, step, output_dir)
+
+            wandb.log(
+                {
+                    'kl_loss': kl_loss,
+                    'reconstruction_loss': reconstruction_loss, 
+                    'loss': loss,
+                    'random_image': wandb.Image(random_image),
+                    'x': wandb.Image(make_pil_image(x[0])),
+                    'x_hat': wandb.Image(make_pil_image(x_hat[0])),
+                    'temperature': temperature,
+                    'learning_rate': learning_rate
+                }, 
+                step=step
+            )
+            step += 1
+            progress_bar.update(1)
         
 
 if __name__ == '__main__': main()
